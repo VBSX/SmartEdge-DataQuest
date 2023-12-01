724e8960d706e0a616a71bd74f8745ed8e057c0d
Vitor <84788940+VBSX@users.noreply.github.com>
2023-12-01 15:51:00 -0300

    UPDATE

--- components/automation_windows/create_post.py
+++ components/automation_windows/create_post.py
@@ -88,8 +88,13 @@ class BrowserController():
         return send_keys
     
     def login_forum(self):
-        logged = self.navegador.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[3]/a")
-        if not logged.is_displayed():
+        try:
+            logged = self.navegador.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[3]/a")
+            is_logged = logged.is_displayed()
+        except:
+            is_logged = False
+            
+        if not is_logged:
             path_label_username = '/html/body/div/div[2]/div/table/tbody/tr[2]/td/form/table/tbody/tr[2]/td[2]/input'
             path_label_passwd = '/html/body/div/div[2]/div/table/tbody/tr[2]/td/form/table/tbody/tr[3]/td[2]/input'
             button_login_path = '/html/body/div/div[2]/div/table/tbody/tr[2]/td/form/table/tbody/tr[4]/td/input'
@@ -112,7 +117,8 @@ class BrowserController():
         
     def login_bitrix(self):
         # verifica se existe um elemento antes de proseguir
-        logged = self.navegador.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td/div/div[2]/div/div[1]/div/div/div[2]/div")
+        logged = self.navegador.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[1]/div[1]/div[2]/div[1]/ul")
+
         if not logged.is_displayed():
             path_email = '/html/body/div[1]/div[2]/div/div[1]/div/div/div[3]/div/form/div/div[1]/div/input'
             path_passwd = '/html/body/div[1]/div[2]/div/div[1]/div/div/div[3]/div/form/div/div[2]/div/div[1]/div/input'
@@ -124,38 +130,51 @@ class BrowserController():
 
     def bitrix_post1 (self):
         path_post_body = '/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td/div/div[2]/div/div[1]/div/div/div[2]/div'
-        self.click_base(path_post_body)
         
+        self.click_base(path_post_body)
+        sleep(0.5)
         # envia para o navegador, lembrar de verificar onde esta o foco antes de mandar o comando
         actions = ActionChains(self.navegador)
-        active_element = self.navegador.execute_script("return document.activeElement;")
-        active_element.send_keys(Keys.CONTROL + "v")
-        active_element.send_keys(Keys.ENTER)
+        actions.key_down(Keys.CONTROL)
+        actions.send_keys("v")
+        actions.key_up(Keys.CONTROL)
         actions.perform()
+        sleep(2)
+        for item in range(0,100):
+            try:
+                element = self.navegador.find_element(By.XPATH, f'/html/body/div[{item}]/span[3]/span[2]')       
+                element.click()
+                print('achou o elemento')
+                break
+            except:
+                print('Não achou o elemento')
+        actions.key_down(Keys.ENTER).perform()
+        actions.key_up(Keys.ENTER).perform()
+        sleep(10)
     
     def bitrix_post_2(self):
         path_open_chat = r'/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[1]/div[1]/div[2]/div[1]/ul/li[3]/ul/li[2]/a'
-        path_chat_searcher = r'/html/body/div[20]/div/div[1]/div/div[1]/div[2]/div/div[1]/div/div[1]/div[2]/div/input'
-        path_chat_searcher2 = r'/html/body/div[42]/div/div[1]/div/div[1]/div[2]/div/div[1]/div/div[1]/div[2]/div/input'
-        path_chat_searcher3  = r'/html/body/div[26]/div/div[1]/div/div[1]/div[2]/div/div[1]/div/div[1]/div[2]/div/input'
-        
-        path_chat = '/html/body/div[26]/div/div[1]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[3]/div/div[1]/div[2]/div[1]/textarea'
-        
+
         self.click_base(path_open_chat)
         text_find_chat = 'canais - avisos'
         sleep(7)
-        list_elements = [path_chat_searcher,path_chat_searcher2, path_chat_searcher3]
-
-        for element in list_elements:
+        for element in range(0,100):
             try:
-                element = self.navegador.find_element(By.XPATH, element)  
+                element = self.navegador.find_element(By.XPATH, f'/html/body/div[{element}]/div/div[1]/div/div[1]/div[2]/div/div[1]/div/div[1]/div[2]/div/input')  
                 element.click()
                 element.send_keys(text_find_chat)
                 element.send_keys(Keys.ENTER)
                 # JA ABERTO O CHAT AGORA IRÁ ESCREVER A MENSAGEM
-                chat = self.click_base(path_chat)
-                chat.send_keys(Keys.CONTROL + "v")
-                chat.send_keys(Keys.ENTER)
+                sleep(1)
+                actions = ActionChains(self.navegador)
+                actions.key_down(Keys.CONTROL).perform()
+                actions.send_keys("v").perform()
+                actions.key_up(Keys.CONTROL).perform()
+                
+                actions.key_down(Keys.ENTER).perform()
+                actions.key_up(Keys.ENTER).perform()
+                
+                sleep(7)
                 break
             except:
                 print('erro', element)
@@ -169,6 +188,9 @@ if __name__ == '__main__':
 Compatível com a versão [b]1.32.13.2132[/b] do [b]MyCommerce PDV[/b]. 
 
 Atenciosamente, Vitor Hugo Borges Dos Santos.""",
-
+bitrix_username='',
+bitrix_passwd='',
+forum_username='',
+forum_passwd=''
 )
 
